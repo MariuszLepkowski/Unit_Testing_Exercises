0cca536acaa299b5481c48c324097b51e9072715
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-02 16:35:16 +0100

    Create tests for calculate_sum

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,6 +3,22 @@ from calculator import calculate_sum
 
 
 class TestCalculator(unittest.TestCase):
-    def test_calculate_cum(self):
+    def test_sum_positive_numbers(self):
         result = calculate_sum(1, 2)
         self.assertEqual(result, 3)
+
+    def test_sum_negative_numbers(self):
+        result = calculate_sum(-3, -7)
+        self.assertEqual(result, -10)
+
+    def test_sum_mixed_numbers(self):
+        result = calculate_sum(5, -2)
+        self.assertEqual(result, 3)
+
+    def test_sum_zero(self):
+        result = calculate_sum(0, 8)
+        self.assertEqual(result, 8)
+
+    def test_sum_both_zeros(self):
+        result = calculate_sum(0, 0)
+        self.assertEqual(result, 0)
