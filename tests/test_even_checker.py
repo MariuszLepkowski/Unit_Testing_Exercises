a9569e878775ca8597d1d1f404aac30a8d26c621
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-02 16:32:10 +0100

    Create new tests for is_even function

--- tests/test_even_checker.py
+++ tests/test_even_checker.py
@@ -3,6 +3,22 @@ from even_checker import is_even
 
 
 class TestEvenChecker(unittest.TestCase):
-    def test_is_even(self):
+    def test_is_even_positive(self):
         result = is_even(2)
         self.assertTrue(result)
+
+    def test_is_even_negative(self):
+        result = is_even(3)
+        self.assertFalse(result)
+
+    def test_is_even_zero(self):
+        result = is_even(0)
+        self.assertTrue(result)
+
+    def test_is_even_negative_even(self):
+        result = is_even(-4)
+        self.assertTrue(result)
+
+    def test_is_even_negative_odd(self):
+        result = is_even(-7)
+        self.assertFalse(result)
